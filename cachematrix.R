826fa5ec6ce53f0b38c3bb2c1a056835e8870a46
Vinko <vinko.kodzoman@fer.hr>
2015-11-21 21:48:08 +0100

    Fully implemented the cahcematrix.R file.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A set of functions for working with matrix and caching 
+## the inverse matrix for increase in computing performance
 
-## Write a short comment describing this function
-
+# Creates a matrix with a full set of setters and getters for the matrix
+# itself and the mean of the matrix as well.
 makeCacheMatrix <- function(x = matrix()) {
-
+	m <- NULL
+	set <- function(y) {
+		x <<- y
+		m <<- NULL
+	}
+	get <- function() x
+	setInverse <- function(solve) m <<- solve
+	getInverse <- function() m
+	list(set = set, get = get,
+		setInverse = setInverse,
+		getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+# Allows the caching of a matrix for latter usage, thus increaing the 
+# performance of a program that uses the inverse matrix value of the 
+# same matrix multiple times
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getInverse()
+        if(!is.null(m)) {
+        	message("getting cahced data")
+        	return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setInverse(m)
+        m
 }
